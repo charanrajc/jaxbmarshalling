d3fd33dde80d9cf72695da47a325a85682585a94
Charan Raj <Charan Raj@DESKTOP-Q5L9UPM>
2018-01-12 00:51:42 +0530

    Create object for JAXB Marshaller.

--- src/com/app/UnmarshallingTest.java
+++ src/com/app/UnmarshallingTest.java
@@ -1,9 +1,27 @@
 package com.app;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+
 public class UnmarshallingTest 
 {
- public static void main(String[] args) 
- {
-	
-}
+	public static void main(String[] args) throws JAXBException 
+	{
+		File file=new File("d:/employee.xsd");
+		JAXBContext context = JAXBContext.newInstance(Employee.class);
+		Unmarshaller unmarshaller=context.createUnmarshaller();
+		Object ob=unmarshaller.unmarshal(file);
+		Employee e= (Employee)ob;
+		System.out.println("id :"+e.getId());
+		System.out.println("name :"+e.getName());
+		System.out.println(" Unmarshalling complted");
+		
+		
+
+		
+	}
 }
