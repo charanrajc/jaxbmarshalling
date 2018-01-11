2e5193b1f28dd7a4c1db0104f7f03b03007431b3
Charan Raj <Charan Raj@DESKTOP-Q5L9UPM>
2018-01-12 00:27:26 +0530

    Added main method and created object for Marshaller

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -1,9 +1,22 @@
 package com.app;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
 public class Test 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws JAXBException 
 	{
-		
+		Employee emp=new Employee();
+		emp.setId(10);
+		emp.setName("charan");
+		JAXBContext context = JAXBContext.newInstance(Employee.class);
+		Marshaller marshaller = context.createMarshaller();
+		File file=new File("D:aa.xml");
+		marshaller.marshal(emp, file);
+		System.out.println("JAXB Marshaller sucessfully done");
 	}
 }
